f681b07a9744c82743cc0b3d437a5a72656d2553
tristanjkay <tristanjkay@gmail.com>
2021-03-08 11:34:20 +0000

    Update geojson.php

--- php/geojson.php
+++ php/geojson.php
@@ -4,14 +4,7 @@
 
     $url='https://www.tristanjkay.com/gazetteer/countryBorders.geo.json';
 
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($ch, CURLOPT_URL,$url);
-
-	$result=curl_exec($ch);
-
-	curl_close($ch);
+	$result = file_get_contents($url);
 
 	$decode = json_decode($result,true);	
 

88351aff7731f26011c4d9dfb082a5c7b897391d
tristanjkay <tristanjkay@gmail.com>
2021-03-12 19:03:48 +0000

    Update worldbank.php

--- php/worldbank.php
+++ php/worldbank.php
@@ -3,7 +3,7 @@ $executionStartTime = microtime(true) / 1000;
 console.log($_REQUEST['country']);
 // build the individual requests, but do not execute them
 $ch_1 = curl_init('http://api.worldbank.org/v2/country/' . $_REQUEST['country'] . '/indicator/SH.ALC.PCAP.LI?date=2019&format=json');
-$ch_2 = curl_init('http://webservice.two.com/');
+$ch_2 = curl_init('http://api.worldbank.org/v2/country/' . $_REQUEST['country'] . '/indicator/BAR.NOED.15UP.ZS?date=2019&format=json');
 curl_setopt($ch_1, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch_2, CURLOPT_RETURNTRANSFER, true);
  

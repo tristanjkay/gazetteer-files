b4e2c7acd2427e29b1d4b660ec40461b616089f9
tristanjkay <tristanjkay@gmail.com>
2021-03-12 20:15:11 +0000

    Update worldbank.php

--- php/worldbank.php
+++ php/worldbank.php
@@ -1,9 +1,8 @@
 <?php
-$executionStartTime = microtime(true) / 1000;
-console.log($_REQUEST['country']);
+
 // build the individual requests, but do not execute them
-$ch_1 = curl_init('http://api.worldbank.org/v2/country/' . $_REQUEST['country'] . '/indicator/SH.ALC.PCAP.LI?date=2019&format=json');
-$ch_2 = curl_init('http://api.worldbank.org/v2/country/' . $_REQUEST['country'] . '/indicator/BAR.NOED.15UP.ZS?date=2019&format=json');
+$ch_1 = curl_init('https://jsonplaceholder.typicode.com/posts');
+$ch_2 = curl_init('https://jsonplaceholder.typicode.com/posts');
 curl_setopt($ch_1, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch_2, CURLOPT_RETURNTRANSFER, true);
  
@@ -26,12 +25,6 @@ curl_multi_close($mh);
 // all of our requests are done, we can now access the results
 $response_1 = curl_multi_getcontent($ch_1);
 $response_2 = curl_multi_getcontent($ch_2);
-
-$decode1 = json_decode($response_1,true)
-$output['data'] = $decode1;
-
-
-echo json_encode($output); 
-
+echo "$response_1 $response_2"; // output results
 
 ?>

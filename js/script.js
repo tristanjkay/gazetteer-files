f892e5073ddd9bbeb861428fd41b6347e02d1d4c
tristanjkay <tristanjkay@gmail.com>
2021-03-13 19:20:17 +0000

    update text

--- js/script.js
+++ js/script.js
@@ -616,6 +616,7 @@ window.onload=function(){
 		function showModal(mycountry) {
 			//document.querySelector('.modal').style.display = "inherit";
 			document.querySelector('.modal-title').innerHTML = mycountry.name;
+			document.querySelector('.continent-title').innerHTML = mycountry.continent;
 			document.querySelector('.col-sm-12').classList.add('col-sm-9');
 			document.querySelector('.col-sm-12.col-sm-9').classList.remove('col-sm-12');
 			col2.style.display = "block";

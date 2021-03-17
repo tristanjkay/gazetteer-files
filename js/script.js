9364e9cf3ccc5001c2c6c1f1ff32765fbe32e337
tristanjkay <tristanjkay@gmail.com>
2021-03-17 14:52:14 +0000

    update buttons

--- js/script.js
+++ js/script.js
@@ -748,6 +748,8 @@ window.onload=function(){
 			$('#climate-temperature').html(selectedCountry.capital.weather.temperature + "&deg");
 			$('#climate-feelslike').html("Feels like " + selectedCountry.capital.weather.feelslike + "&deg");
 			$('#currency').html(selectedCountry.currency);
+			
+			//TODO Make this work
 			$('#exchangeratevalue').html(selectedCountry.exchangerate);
 
 			try {

ad9616d9bfad600e49936b6d1c46c559d6049fce
tristanjkay <tristanjkay@gmail.com>
2021-03-20 12:42:58 +0000

    Update pointsofinterest.php

--- php/pointsofinterest.php
+++ php/pointsofinterest.php
@@ -5,7 +5,7 @@
 	$url ='https://www.triposo.com/api/20210317/poi.json?location_id=' . $_REQUEST['country'] . '&count=1&fields=id,name,score,intro,tag_labels,best_for&order_by=-score&account=5H84J8Q3&token=kkuxteg67z108qmihlu82c7vks8mhfw3'
 
 	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 	curl_setopt($ch, CURLOPT_URL,$url);
 

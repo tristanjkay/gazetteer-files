d6ac5f34e1cffb31104bf71f71bed3329d517bc6
tristanjkay <tristanjkay@gmail.com>
2021-03-30 12:41:19 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -366,7 +366,7 @@ window.onload=function(){
 				},
 				success: function(result) {
 
-					//console.log(result);
+					console.log(result);
 
 					if (result.status.name == "ok") {
 						selectedCountry.news = result['data']['articles']

05c718f8438e517d7d97c8a684b8d55f7b91f817
tristanjkay <tristanjkay@gmail.com>
2021-03-30 12:42:43 +0100

    Update news.php

--- php/news.php
+++ php/news.php
@@ -2,7 +2,7 @@
 
 	$executionStartTime = microtime(true) / 1000;
 
-    $url='http://newsapi.org/v2/everything?q=' . $_REQUEST['country'] . '&from=2021-02-15&sortBy=publishedAt&apiKey=f6c3d2f0f0bd4b27860fb2d9fc938d75';
+    $url='http://newsapi.org/v2/everything?q=' . $_REQUEST['country'] . '&from=2021-03-15&sortBy=publishedAt&apiKey=f6c3d2f0f0bd4b27860fb2d9fc938d75';
 
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);

1bf4cbd6a5bf04c40acac49f834c8507d9af32ae
tristanjkay <tristanjkay@gmail.com>
2021-05-12 16:09:49 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -133,6 +133,7 @@ window.onload=function(){
 			col1.classList.remove('col-sm-3');
 
 			$(".col-sm-3").html(divPrevContent);
+			map.setView([selectedCountry.capital.location[0], selectedCountry.capital.location[1]], 6);
 		});
 		
 		//Add Data from APIs

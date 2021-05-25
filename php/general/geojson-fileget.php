8e47b0539db8a60ecce6f583fa610b747df10fd5
tristanjkay <tristanjkay@gmail.com>
2021-05-25 10:17:53 +0100

    Update geojson-fileget.php

--- php/general/geojson-fileget.php
+++ php/general/geojson-fileget.php
@@ -2,7 +2,7 @@
 
 	$executionStartTime = microtime(true) / 1000;
 
-    $url='https://www.tristanjkay.com/gazetteer/countryBorders.geo.json';
+    $url='https://raw.githubusercontent.com/tristanjkay/gazetteer/main/countryBorders.geo.json?token=AD62AGHTO2Z55HISWIGZRFDAVTAMU';
 
 	$result = file_get_contents($url);
 
